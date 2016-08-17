62752afac4a7909b83a0f705797d485e6c6b8cff
Cang Ta <hoksilato176@gmail.com>
2016-08-17 20:41:14 +0700

    finish milestone 3

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,24 @@
+class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to root_path, notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created, location: root_path(@product) }
+      else
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :image_path, :image)
+  end
+end
